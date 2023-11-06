cf3da4413b2aa89e11d704d24e501b4551820f4f
maheribrahem <53615611+maheribrahem@users.noreply.github.com>
2023-11-06 11:20:14 +0200

    Add files via upload

---
+++ lib/extra/privacy_and_policy/privacy_and_policy_widget.dart
@@ -0,0 +1,84 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'privacy_and_policy_model.dart';
+export 'privacy_and_policy_model.dart';
+
+class PrivacyAndPolicyWidget extends StatefulWidget {
+  const PrivacyAndPolicyWidget({super.key});
+
+  @override
+  _PrivacyAndPolicyWidgetState createState() => _PrivacyAndPolicyWidgetState();
+}
+
+class _PrivacyAndPolicyWidgetState extends State<PrivacyAndPolicyWidget> {
+  late PrivacyAndPolicyModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => PrivacyAndPolicyModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isiOS) {
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarBrightness: Theme.of(context).brightness,
+          systemStatusBarContrastEnforced: true,
+        ),
+      );
+    }
+
+    context.watch<FFAppState>();
+
+    return Title(
+        title: 'privacyAndPolicy',
+        color: FlutterFlowTheme.of(context).primary.withAlpha(0XFF),
+        child: GestureDetector(
+          onTap: () => _model.unfocusNode.canRequestFocus
+              ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+              : FocusScope.of(context).unfocus(),
+          child: Scaffold(
+            key: scaffoldKey,
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            body: SafeArea(
+              top: true,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  RichText(
+                    textScaleFactor: MediaQuery.of(context).textScaleFactor,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: FFLocalizations.of(context).getText(
+                            'c8ii3k96' /* بعد أن وصلتنا الكثير من طلبات ... */,
+                          ),
+                          style: const TextStyle(),
+                        )
+                      ],
+                      style: FlutterFlowTheme.of(context).bodyMedium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
